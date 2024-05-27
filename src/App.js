4d6c3c9f887883bdb0df44d2bc23312f97a590b0
Nguyen Son <85403817+Ntsfullstack@users.noreply.github.com>
2024-05-27 13:35:55 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes } from "react-router-dom";
-// import "./App.css";
+import "./App.css";
 import RoutersManager from "./routers/index";
-
+import useDarkMode from "./darkmode";
+import { useSelector } from "react-redux";
 
 function App() {
+  const currentTheme = useSelector((state) => state.Movie.mode);
+  useEffect(() => {
+    document.body.setAttribute("data-theme", currentTheme);
+  }, [currentTheme]);
+
   if (false) {
     // Removed debugging statement
     return null; // Improved readability by explicitly returning null
